fffc4b1957258990d0ddef947dc5ac1a9e206f11
MD Ahsan Habib Rasel <jucse28.367@gmail.com>
2023-03-26 00:55:21 +0600

    This code remove duplicate charaters

--- Remove_duplicate_char.java
+++ Remove_duplicate_char.java
@@ -1,17 +1,27 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Remove_duplicate_char {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
         String s=scanner.nextLine();
         String ans="";
+        ConcurrentHashMap<Character,Integer>mp=new ConcurrentHashMap<>();
         for(int i=0;i<s.length();i++)
         {
             char ch=s.charAt(i);
-            int dd=ans.indexOf(ch);
-            if(dd==-1)
+            int val=0;
+            if(mp.containsKey(ch))
+            {
+                val=mp.get(ch);
+
+            }
+            val++;
+            mp.put(ch,val);
+
+            if(val==1)
             {
                 ans=ans+ch;
             }
